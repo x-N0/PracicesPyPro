797b952983360bbafd305f48dcdf2ddbc149b0fe
Robinson Chaplain <51316342+x-n0@users.noreply.github.com>
2020-06-24 15:08:14 -0300

    First Commit from xN0, branch based on my own solution for the tasks.

--- ch09/complexity.py
+++
@@ -1,9 +0,0 @@
-def has_long_words(sentence):
-    if isinstance(sentence, str):  # <1>
-        sentence = sentence.split(' ')
-
-    for word in sentence:  # <2>
-        if len(word) > 10:  # <3>
-            return True
-
-    return False  # <4>
